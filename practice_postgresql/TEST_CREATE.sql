3bc991260161226c6fabf40fc777d58e4a1435a4
jin <datatype2100@gmail.com>
2023-12-04 22:37:43 +0900

    practice postgresql

---
+++ practice_postgresql/TEST_CREATE.sql
@@ -0,0 +1,7 @@
+CREATE TABLE teachers(
+	id bigserial,
+	name varchar(50),
+	school varchar(50),
+	hire_date date,
+	salary numeric
+);
---
+++ practice_postgresql/TEST_INSERT.sql
@@ -0,0 +1,3 @@
+INSERT INTO teachers (name, school, hire_date, salary)
+VALUES ('홍길동', '서울대학교', '2020-01-02', 36200),
+		('이순산', '고려대학교', '2019-01-06', 65000);
